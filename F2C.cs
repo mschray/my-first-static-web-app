802d5ca3bd1605d651efa938531e4cf95fd440dc
mschrayms <mschray@microsoft.com>
2021-11-15 20:39:35 -0600

    Change client to use endpoint
    Change client to use endpoint

--- F2C.cs
+++ F2C.cs
@@ -19,15 +19,18 @@ namespace ITT.ITMD419519
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            string temp = req.Query["temp"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            temp = temp ?? data?.name;
 
-            string responseMessage = string.IsNullOrEmpty(name)
+            var tempAsNumber = Convert.ToDouble(temp);
+            var convertedTemp = (tempAsNumber - 32) * 5 / 9;
+
+            string responseMessage = string.IsNullOrEmpty(temp)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+                : $"{convertedTemp}";
 
             return new OkObjectResult(responseMessage);
         }
