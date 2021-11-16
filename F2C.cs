a2937c3f0fe4cc06120c98a3b13ffd1a4c68341f
mschrayms <mschray@microsoft.com>
2021-11-15 20:10:02 -0600

    recreate
    Move files to API Dir

--- F2C.cs
+++ F2C.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
-namespace ITMD419518.F2C
+namespace ITT.ITMD419519
 {
     public static class F2C
     {
